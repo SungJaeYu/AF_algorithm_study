97cf8c3002a795309f0559544de2c8a06b41b97b
SungJaeYu <ysj8781@gmail.com>
2022-09-03 14:36:44 +0900

    postfix

--- week7/postfix.py
+++ week7/postfix.py
@@ -1,13 +1,16 @@
 import sys
 
-OPERATOR = ['+', '-', '*', '/']
+OPERATOR_LOW = ['+', '-']
+OPERATOR_HIGH = ['*', '/']
 BRACKET = ['(', ')']
 stack = []
 output = []
 def main():
     calculation = [*sys.stdin.readline().strip()]
     for c in calculation:
-        if c in OPERATOR:
+        if c in OPERATOR_LOW:
+           if stack[-1] in OPERATOR_HIGH:
+               stack.pop()
            stack.append(c)
         elif c in BRACKET:
             pass
