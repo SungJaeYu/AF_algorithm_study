7d6e15a33214aaed3f705644b1c992e0e5a480a0
SungJaeYu <ysj8781@gmail.com>
2022-09-03 22:19:46 +0900

    make func dict

--- week7/postfix.py
+++ week7/postfix.py
@@ -1,44 +1,49 @@
 import sys
 
-OPERATOR_LOW = ['+', '-']
-OPERATOR_HIGH = ['*', '/']
-OPEN_BRACKET = '('
-CLOSE_BRACKET = ')'
+OPERATOR = ['+', '-', '*', '/', '(', ')']
 stack = []
-output = []
+
+proc_func = {'+': proc_operator_low,
+             '-': proc_operator_low,
+             '*': proc_operator_high,
+             '/': proc_operator_high,
+             '(': proc_open_bracket,
+             ')': proc_close_bracket,
+             }
+
+def proc_operator_low(c):
+    while len(stack) != 0:
+        if stack[-1] == OPEN_BRACKET:
+            break;
+        print(stack.pop(), end='')
+    stack.append(c)
+
+def proc_operator_high(c):
+    while len(stack) != 0:
+        if stack[-1] not in OPERATOR_HIGH:
+            break;
+        print(stack.pop(), end='')
+    stack.append(c)
+
+def proc_open_bracket(c):
+    stack.append(c)
+
+def proc_close_bracket(c):
+     while len(stack) != 0:
+        temp = stack.pop()
+        if temp == OPEN_BRACKET:
+            break;
+        print(temp, end='')
+
 
 def main():
     calculation = [*sys.stdin.readline().strip()]
     for c in calculation:
-        if c in OPERATOR_LOW:
-           while len(stack) != 0:
-               if stack[-1] == OPEN_BRACKET:
-                   break
-               temp = stack.pop()
-               print(temp, end='')
-               
-           if c in OPERATOR_LOW:        
-               stack.append(c)
-        elif c == CLOSE_BRACKET:
-            while len(stack) != 0:
-                temp = stack.pop()
-                if temp == OPEN_BRACKET:
-                    break;
-                print(temp, end='')
-        elif c == OPEN_BRACKET:
-            stack.append(c)
-        elif c in OPERATOR_HIGH:
-            while len(stack) != 0:
-                if stack[-1] not in OPERATOR_HIGH:
-                    break;
-                temp = stack.pop()
-                print(temp, end='')
-            stack.append(c)
-        else:
+        if c in OPERATOR:
+            proc_func[c](c)
+        else: # Alphabet
             print(c, end='')
     while len(stack) != 0:
-        temp = stack.pop()
-        if temp != OPEN_BRACKET:
-            print(temp, end='')
+        print(stack.pop(), end='')
 
 main()
