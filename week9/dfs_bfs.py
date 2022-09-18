8cb902aaa3d279ec5c222496ec1ce537b88e2eb8
SungJaeYu <ysj8781@gmail.com>
2022-09-18 20:31:33 +0900

    dfs bfs first done

---
+++ week9/dfs_bfs.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+
+def dfs(v, edges, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for vertex in edges[v]:
+        if not visited[vertex]:
+            dfs(vertex, edges, visited)
+
+def bfs(v, edges, visited):
+    queue = deque([v])
+    visited[v] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in edges[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+def main():
+    n, m, v = map(int, sys.stdin.readline().split())
+    visited = [False] * (n + 1)
+    edges = []
+    for i in range(n + 1):
+        edges.append([])
+    for i in range(m):
+        start, end = map(int, sys.stdin.readline().split())
+        edges[start].append(end)
+        edges[end].append(start)
+    dfs(v, edges, visited)
+    print('')
+    visited = [False] * (n + 1)
+    bfs(v, edges, visited)
+
+main()
