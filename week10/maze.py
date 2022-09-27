4cd42f5b5993bb1ccdfeae3cfa9c85d6761f6205
SungJaeYu <ysj8781@gmail.com>
2022-09-27 20:31:45 +0900

    maze done

--- week10/maze.py
+++ week10/maze.py
@@ -1,41 +1,67 @@
 import sys
 
+def input_maze_size():
+    N, M = map(int, sys.stdin.readline().split())
+    return N, M
 
-def dfs(v, graph, visited):
-    visited[v] = True
-    print(v)
-    for vertex in graph[v]:
-        if not visited[vertex]:
-            dfs(vertex, graph, visited)
+class Maze():
+    def __init__(self, N, M):
+        self.size = (N, M)
+        self.graph = dict()
+        self.visited = set()
+        self.destination = (N-1, M-1)
+        self.start = (0, 0)
+
+    def input_maze(self):
+        N, M = self.size
+        self.map = []
+        for _ in range(N):
+            row = sys.stdin.readline().strip()
+            row_list = list(row)
+            self.map.append(row_list)
+
+    def convert_maze_to_graph(self):
+        N, M = self.size
+        for i in range(N):
+            for j in range(M):
+                if self.map[i][j] == '1':
+                    position = (i, j)
+                    adj = []
+                    for neighbor in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                        x, y = neighbor
+                        if x < 0 or y < 0 or x >= N or y >= M:
+                            continue
+                        if self.map[x][y] == '1':
+                            adj.append(neighbor)        
+                    self.graph[position] = adj
+
+    def bfs(self):
+        queue = []
+        queue.append([self.start, 0])
+        self.visited.add(self.start)
+        while queue:
+            position, distance = queue.pop()
+            new_distance = distance + 1
+            for vertex in self.graph[position]:
+                if vertex not in self.visited:
+                    if vertex == self.destination:
+                        return new_distance
+                    self.visited.add(vertex)
+                    queue.append([vertex, new_distance])
+        assert(0)
+
+    def maze_search(self):
+        distance = self.bfs()
+        node_num = distance + 1
+        print(node_num)
 
 
-def maze_search():
-    n, m = map(int, sys.stdin.readline().split())
-    maze = []
-    for _ in range(n):
-        row = sys.stdin.readline().strip()
-        row_list = list(row)
-        maze.append(row_list)
-    
-    visited = dict() 
-    graph = dict()
-    for i in range(n):
-        for j in range(m):
-            if maze[i][j] == '1':
-                position = (i, j)
-                visited[position] = False
-                graph[position] = []
-                if (i-1, j) in graph:
-                    graph[(i-1, j)].append(position)
-                if (i, j-1) in graph:
-                    graph[(i, j-1)].append(position)
-                if (i+1, j) in graph:
-                    graph[(i+1, j)].append(position)
-                if (i, j+1) in graph:
-                    graph[(i, j+1)].append(position)
-    start = (0, 0)
-    dfs(start, graph, visited)
-     
-maze_search()           
+def main():
+    N, M = input_maze_size()
+    maze = Maze(N, M)
+    maze.input_maze()
+    maze.convert_maze_to_graph()
+    maze.maze_search()
 
-   
+main()
+
