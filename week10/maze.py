a8b0cb6761e99c21aacfcf5e60f21a0e62c586a7
SungJaeYu <ysj8781@gmail.com>
2022-10-02 12:49:31 +0900

    delete graph and convert to graph function because they don't need

--- week10/maze.py
+++ week10/maze.py
@@ -9,56 +9,33 @@ def input_maze():
         row.append('0')
         maze.append(row)
     maze.append(['0']* (M + 2))
-    print(maze)
     return N, M, maze
 
-class Maze():
-    def __init__(self, N, M, maze_map):
-        self.size = (N, M)
-        self.graph = dict()
-        self.visited = set()
-        self.destination = (N, M)
-        self.start = (1, 1)
-        self.map = maze_map
 
-    def convert_maze_to_graph(self):
-        N, M = self.size
-        for i in range(1, N + 1):
-            for j in range(1, M + 1):
-                if self.map[i][j] == '1':
-                    adj = []
-                    neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
-                    for neighbor in neighbors:
-                        x, y = neighbor
-                        if self.map[x][y] == '1':
-                            adj.append(neighbor)        
-                    self.graph[(i, j)] = adj
-
-    def bfs(self):
-        queue = []
-        queue.append([self.start, 0])
-        self.visited.add(self.start)
-        while queue:
-            position, distance = queue.pop(0)
-            new_distance = distance + 1
-            for vertex in self.graph[position]:
-                if vertex not in self.visited:
-                    if vertex == self.destination:
-                        return new_distance
-                    self.visited.add(vertex)
-                    queue.append([vertex, new_distance])
-
-    def maze_search(self):
-        distance = self.bfs()
-        node_num = distance + 1
-        print(node_num)
+def bfs(N, M, maze_map):
+    start = (1, 1)
+    destination = (N, M)
+    visited = set()
+    queue = [[start, 0]]
+    visited.add(start)
+    while queue:
+        position, distance = queue.pop(0)
+        new_distance = distance + 1
+        (i, j) = position
+        neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
+        for neighbor in neighbors:
+            (n, m) = neighbor
+            if neighbor not in visited and maze_map[n][m] == '1':
+                if neighbor == destination:
+                    return new_distance
+                visited.add(neighbor)
+                queue.append([neighbor, new_distance])
 
 
 def main():
     N, M, maze_map = input_maze()
-    maze = Maze(N, M, maze_map)
-    maze.convert_maze_to_graph()
-    maze.maze_search()
+    result = bfs(N, M, maze_map)
+    print(result + 1)
 
 main()
 
