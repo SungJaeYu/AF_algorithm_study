c33c03fee29bc920333c4d47899c0a0243663bc0
SungJaeYu <ysj8781@gmail.com>
2022-10-09 22:16:12 +0900

    just ab using dict

--- week12/4sum0.py
+++ week12/4sum0.py
@@ -8,34 +8,23 @@ def input_data():
         abcd.append(row)
     return n, abcd
 
-def sum_two_list(n, abcd):
+def main():
+    count = 0
+    n, abcd = input_data()
     ab_sum = dict()
-    cd_sum = dict()
     for i in range(n):
         for j in range(n):
             ab = abcd[i][0] + abcd[j][1]
-            cd = abcd[i][2] + abcd[j][3]
             if ab in ab_sum:
                 ab_sum[ab] += 1
             else:
                 ab_sum[ab] = 1
 
-            if cd in cd_sum:
-                cd_sum[cd] += 1
-            else:
-                cd_sum[cd] = 1
-
-    return ab_sum, cd_sum
-
-def main():
-    result = 0
-    n, abcd = input_data()
-    ab_sum, cd_sum = sum_two_list(n, abcd)
-    bound = 0
-    for ab, count in ab_sum.items():
-        if -ab in cd_sum:
-            result += count * cd_sum[-ab] 
-    
-    print(result)
+    for i in range(n):
+        for j in range(n):
+            cd = abcd[i][2] + abcd[j][3]
+            if -cd in ab_sum:
+                count += 1
+    print(count)
 
 main()
